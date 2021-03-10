23ffee87f760d6e0ae36b65d6004db72f69021be
Chung Him Kwok <tonykwok730@Chungs-MacBook-Air.local>
2021-03-10 15:35:11 +0800

    first commit

---
+++ src/pages/SignUp.js
@@ -0,0 +1,13 @@
+import { ReactComponent } from "*.svg";
+import React from "react";
+
+class SignUp extends ReactComponent {
+
+    render() {
+        return  <div> 
+                    <h1>Hello World</h1>  
+                </div>
+    }
+}
+
+export default SignUp;
